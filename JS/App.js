aa172df4055e136459386930d52976c49e7e3411
RjPorosh <PoroshScientist@outlook.com>
2019-09-13 15:54:29 +0600

    First Commit Paid Theme

---
+++ JS/App.js
@@ -0,0 +1,20 @@
+/**For another Nav Manu**/
+$(document).ready(function(){
+$(".nav-icon").click(function(){
+    $(".full-nav").addClass("open")
+});
+$(".nav-close").click(function(){
+    $(".full-nav").removeClass("open")
+});
+});
+/****/
+$(window).scroll(function () { 
+    var sc=$(window).scrollTop();
+    if(sc>100)
+    {
+        $(".nav").addClass("sticky");
+    }
+    else{
+        $(".nav").removeClass("sticky");
+    }
+});
